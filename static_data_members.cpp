858e21a74a3a3302382790152e9adf03dd610a08
akshatverma1 <akshaverma14@gmail.com>
2023-12-12 14:41:56 +0530

    Updated files of c++

--- static_data_members.cpp
+++ static_data_members.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 using namespace std;
-
 class users{
     private :
-    static int count;
+    static 
+    int count;
     char name;
     int roll;
     public :
     void getdata(void);
     void setdata(void);
+
+    static void func(){
+        cout<<"The Value of count is "<<count<<endl;
+    }
 };
-int users :: count;
+int users :: count =2000;
 void users :: setdata(){
     cout<<"Enter rollno of student"<<endl;
     cin>>roll;
@@ -24,9 +28,12 @@ int main(){
     users akshat,lakshya,utkarsh;
     akshat.setdata();
     akshat.getdata();
+    users :: func();
     lakshya.setdata();
     lakshya.getdata();
+    users :: func();
     utkarsh.setdata();
     utkarsh.getdata();
+    users :: func();
     return 0;
 }
