9b73a14fb14e0d063889384e772ebf262b2df204
akshatverma1 <akshaverma14@gmail.com>
2023-12-13 17:33:18 +0530

    13/12/2023

---
+++ constructor.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class akshat{
+    int a,b;
+    public :
+        akshat (void);
+        void setdata(){
+            cout<<a<<" & "<<b;
+        }
+};
+akshat :: akshat(void){
+    a=0;
+    b=2;
+}
+int main(){
+    akshat t;
+    t.setdata();    
+    return 0;
+}
