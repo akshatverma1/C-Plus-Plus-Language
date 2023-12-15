525889ee4349fe0d43d08169a9788af52ac24741
akshatverma1 <akshaverma14@gmail.com>
2023-12-15 17:59:36 +0530

    15/12/2023

---
+++ destructor.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class electron{
+    private :
+    int a =1;
+    public :
+        electron(){
+            a++;
+            cout<<"Constructor invoked "<<a<<endl;
+        }
+        ~electron(){
+            cout<<"destructor call "<<a<<endl;
+            a--;
+            }
+};
+int main(){
+    electron a;{
+        cout <<"First Object"<<endl;
+        electron s,r;
+        cout <<"Two Object call"<<endl;
+    }
+    electron b;
+    {
+        cout <<"second Object"<<endl;
+    }
+    return 0;
+}
